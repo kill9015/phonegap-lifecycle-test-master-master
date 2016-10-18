366f99553e80afaac2d0f008181f3db31ef23084
kill9015 <peab1_15@uni.worc.ac.uk>
2016-10-18 11:06:15 +0100

    gave up
    changed the code so that it will work but is very inefficent

--- www/js/index.js
+++ www/js/index.js
@@ -17,8 +17,16 @@ var key = "phone"; */
         window.localStorage.setItem("wallet", "black");
         window.localStorage.setItem("USB", "red");
        
-        var calling = window.localStorage.getItem("pen", "phone", "keys", "wallet", "USB");
+        var calling = window.localStorage.getItem("pen");
+        var calling2 = window.localStorage.getItem("phone");
+        var calling3 = window.localStorage.getItem("keys");
+        var calling4 = window.localStorage.getItem("wallet");
+        var calling5 = window.localStorage.getItem("USB");
         window.alert(calling);
+        window.alert(calling2);
+        window.alert(calling3);
+        window.alert(calling4);
+        window.alert(calling5);
     }
 	
 	function updateDisplay() {
